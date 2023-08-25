9976106a4fb3a701c6530c38657bace7f3092c38
adostacey <aaron.stacey@proton.me>
2023-08-25 11:09:55 -0500

    Add playRound fn

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function getComputerChoice() {
   const SEED = Math.floor(Math.random() * WEAPONS.length);
   return WEAPONS[SEED];
 }
+
+function playRound(playerSelection, computerSelection) {
+  const PLAYER = playerSelection.toLowerCase();
+  const COMPUTER = computerSelection;
+
+  if (PLAYER === COMPUTER) {
+    return "It's a Draw!";
+  }
+
+  if (PLAYER === "rock" && COMPUTER === "scissors") {
+    return `You win! ${PLAYER} beats ${COMPUTER}`;
+  } else if (PLAYER === "paper" && COMPUTER === "rock") {
+    return `You win! ${PLAYER} beats ${COMPUTER}`;
+  } else if (PLAYER === "scissors" && COMPUTER === "paper") {
+    return `You win! ${PLAYER} beats ${COMPUTER}`;
+  } else {
+    return `You lose! ${COMPUTER} beats ${PLAYER}`;
+  }
+}
